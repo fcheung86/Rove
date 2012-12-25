64c05e8379ebb1926b3a537a84eb2841c3a78a44
pthieu <pthieu@gmail.com>
2012-12-25 03:34:26 -0500

    Adding in preliminary splash screen

---
+++ Rove/src/com/fourkins/rove/activity/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.fourkins.rove.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.fourkins.rove.R;
+
+public class SplashScreen extends Activity {
+
+    protected int _splashTime = 5000;
+    private Thread splashThread;
+
+    // Trying to make a splash screen here
+    @Override
+    // wtf is a savedInstanceState
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Apparently need to use activity_main, want to use something else
+        setContentView(R.layout.splashscreen);
+
+        splashThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        // 5s wait
+                        wait(_splashTime);
+                    }
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    // NOTE: Splashscreen.this because current context is set to run() function
+                    Intent myIntent = new Intent(SplashScreen.this, com.fourkins.rove.activity.MainActivity.class);
+                    startActivity(myIntent);
+
+                }
+            }
+        };
+        splashThread.start();
+    }
+
+    // Kill splashscreen on tap
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            synchronized (splashThread) {
+                splashThread.notifyAll();
+            }
+        }
+        return true;
+    }
+}
